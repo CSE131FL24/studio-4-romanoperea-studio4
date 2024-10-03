ddef4ef95d0db8e43e7e7a29eed3e774354ec9e6
chris <chris@Jeggaza.dhcp.wustl.edu>
2024-10-03 12:30:48 -0500

    This is what I did to make the interpret drawing file work.

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,61 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		
+		String shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+
+				
+		if(shape.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if(isFilled == true) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+		else if(shape.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double semiMajorAxis = in.nextDouble();
+			double semiMinorAxis = in.nextDouble();
+		
+			if(isFilled == true) {
+				StdDraw.filledEllipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+			else {
+				StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+		}
+		else if(shape.equals("triangle")) {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			double [] x = {x1, x2, x3};
+			double [] y = {y1, y2, y3};
+			
+			if(isFilled == true) {
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				StdDraw.polygon(x, y);
+
+			}
+		}
+
 	}
 }
